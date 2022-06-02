e6b4625049462cbcbf0a6521b6f16580c298998b
hovertank3d <iskrim@iskrim.xyz>
2022-06-03 01:42:24 +0200

    xd

--- commands.go
+++ commands.go
@@ -21,6 +21,11 @@ var commands = [...]command{
 	{"ping", ping},
 	{"chatid", chatid},
 	{"syrok", syrok},
+	{"syrok_mono", syrok_mono},
+	{"syrok_color", syrok_color},
+	{"syrok_and", syrok_and},
+	{"syrok_xor", syrok_xor},
+	{"syrok_or", syrok_or},
 }
 
 func execCommand(update tgbotapi.Update, bot *tgbotapi.BotAPI) string {
@@ -43,21 +48,43 @@ func chatid(update tgbotapi.Update, bot *tgbotapi.BotAPI) string {
 }
 
 func syrok(update tgbotapi.Update, bot *tgbotapi.BotAPI) string {
+	return _syrok(update, bot, -1)
+}
+func syrok_mono(update tgbotapi.Update, bot *tgbotapi.BotAPI) string {
+	return _syrok(update, bot, 0)
+}
+func syrok_color(update tgbotapi.Update, bot *tgbotapi.BotAPI) string {
+	return _syrok(update, bot, 1)
+}
+func syrok_and(update tgbotapi.Update, bot *tgbotapi.BotAPI) string {
+	return _syrok(update, bot, 2)
+}
+func syrok_xor(update tgbotapi.Update, bot *tgbotapi.BotAPI) string {
+	return _syrok(update, bot, 3)
+}
+func syrok_or(update tgbotapi.Update, bot *tgbotapi.BotAPI) string {
+	return _syrok(update, bot, 4)
+}
+
+func _syrok(update tgbotapi.Update, bot *tgbotapi.BotAPI, mode int) string {
 	var compressFile bool
 	var file string
 	if update.Message.Text == "" {
 		return "kek"
 	}
+
 	reply := update.Message.ReplyToMessage
-	if reply == nil || (reply.Photo == nil && reply.Document == nil) {
+	if reply == nil || (reply.Photo == nil && reply.Document == nil && reply.Sticker == nil) {
 		return "lol"
 	}
-	compressFile = reply.Document != nil
+	compressFile = reply.Document != nil || reply.Sticker != nil
 
-	if !compressFile {
+	if reply.Photo != nil {
 		file = reply.Photo[len(reply.Photo)-1].FileID
-	} else {
+	} else if reply.Document != nil {
 		file = reply.Document.FileID
+	} else if reply.Sticker != nil {
+		file = reply.Sticker.FileID
 	}
 
 	link, err := bot.GetFileDirectURL(file)
@@ -74,8 +101,9 @@ func syrok(update tgbotapi.Update, bot *tgbotapi.BotAPI) string {
 	if err != nil {
 		return err.Error()
 	}
-	mode := 0
-	mode, _ = strconv.Atoi(update.Message.CommandArguments())
+	if mode < 0 {
+		mode, _ = strconv.Atoi(update.Message.CommandArguments())
+	}
 
 	data, err := syrokImage(bytes, mode)
 	if err != nil {
